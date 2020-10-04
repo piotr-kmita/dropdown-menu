33a8f7b6ccf9333a08f8d90b59ad0f4c2250a84a
Piotr Kmita <piotr@szkicownik.com>
2020-10-05 00:11:14 +0200

    Add loaders in webpack.dev.js
    Add loaders: babel, css, postcss and style

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,4 +7,26 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, "./dist")
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+            }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+         'style-loader',
+         'css-loader',
+         'postcss-loader', 
+        ]
+      },
+    ],
+  },
 }
